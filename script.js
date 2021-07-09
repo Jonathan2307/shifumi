91723e061b2699c988e358173e051b2774c2703c
Jonathan GRATIGNY <79470112+Jonathan2307@users.noreply.github.com>
2021-07-09 13:52:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function shifumi() {
     const user_choice = ['rock', 'paper', 'scissors']
-    const choice = prompt(`Choose one of the following : ${user_choice.join(', ')}.`)
-    if (user_choice.includes(choice)) {
-        alert(`Congratulations, you chose ${choice}`)
-    } else {
-        alert(`you did not choose one of the ${user_choice.length} choices`)
-    }
-}
-shifumi();
+    let choice = prompt(`Choose one of the following : ${user_choice.join(', ')}.`)
+    if (!user_choice.includes(choice)) {
+        alert(`you did not choose one of the ${user_choice.length} choices`);
+        choice = prompt(`Choose one of the following : ${user_choice.join(', ')}, last chance!`)
+        if(user_choice.includes(choice)) {
+            alert(`Congratulations, you chose ${choice}`);
+        } else {
+            alert(`${choice} isn't one of the three choices, game is aborted.`)
+        }
+        return choice;
+    }}
+    shifumi();
