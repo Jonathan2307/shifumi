df5f708caafb204100aee3b4caa2d7b640d6579c
Jonathan GRATIGNY <79470112+Jonathan2307@users.noreply.github.com>
2021-07-12 14:12:53 +0200

    Add files via upload
    updated JS 8)

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function shifumi() {
     let choice = prompt(`Choose one of the following : ${user_choice.join(', ')}.`)
     if (!user_choice.includes(choice)) {
         alert(`Attention, ${choice} is not one of the ${user_choice.length} choices`);
-        return shifumi()
-        if (user_choice.includes(choice)) {
-            alert(`Congratulations, you chose ${choice}`);
-        }
+        shifumi()
+    }
+    if (user_choice.includes(choice)) {
+        alert(`Congratulations, you chose ${choice}`);
     }
     return choice;
 }
