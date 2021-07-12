529ab0ada0958c3fb156067f7f295a7074d1dc5f
Jonathan GRATIGNY <79470112+Jonathan2307@users.noreply.github.com>
2021-07-12 10:13:35 +0200

    Add files via upload
    just a small fix :)

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function shifumi() {
     const user_choice = ['rock', 'paper', 'scissors']
-    const choice = prompt(`Choose one of the following : ${user_choice.join(', ')}.`)
-    if (user_choice.includes(choice)) {
-        alert(`Congratulations, you chose ${choice}`)
-    } else {
-        alert(`you did not choose one of the ${user_choice.length} choices`)
+    let choice = prompt(`Choose one of the following : ${user_choice.join(', ')}.`)
+    if (!user_choice.includes(choice)) {
+        alert(`Attention, ${choice} is not one of the ${user_choice.length} choices`);
+        return shifumi()
+        if (user_choice.includes(choice)) {
+            alert(`Congratulations, you chose ${choice}`);
+        }
     }
+    return choice;
 }
 shifumi();
