b0771ac25b9f87d923bb1a71791c48ab0c885bc2
Jonathan GRATIGNY <79470112+Jonathan2307@users.noreply.github.com>
2021-07-14 20:22:58 +0200

    Add files via upload
    delete the last condition and hope this will works 🤞

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ function shifumi() {
     let choice = prompt(`Choose one of the following : ${user_choice.join(', ')}.`)
     if (!user_choice.includes(choice)) {
         alert(`Attention, ${choice} is not one of the ${user_choice.length} choices`);
-        shifumi()
+        return shifumi()
     }
-    if (user_choice.includes(choice)) {
-        alert(`Congratulations, you chose ${choice}`);
-    }
-    return choice;
+    alert(`Congratulations, you chose ${choice}`);
 }
 shifumi();
