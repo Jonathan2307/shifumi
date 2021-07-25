ca4bb1f090ff97947c12bbe0536b64b62ba40028
Jonathan GRATIGNY <79470112+Jonathan2307@users.noreply.github.com>
2021-07-25 21:15:26 +0200

    add part 3 - Generate a random choice from a IA

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function shifumi() {
-    const user_choice = ['rock', 'paper', 'scissors']
-    const choice = prompt(`Choose one of the following: ${user_choice.join(', ')}.`)
+    const user_choice = ['rock', 'paper', 'scissors'];
+    const choice = prompt(`Choose one of the following: ${user_choice.join(', ')}.`);
 
     if (!user_choice.includes(choice)) {
         alert(`Attention, ${choice} is not one of the ${user_choice.length} choices`);
@@ -8,7 +8,12 @@ function shifumi() {
     }
 
     alert(`Congratulations, you chose ${choice}`);
-}
 
+    //AI choice
+    const ia = Math.floor(Math.random() * user_choice.length)
+    const ia_choice = user_choice[ia]
+    alert(`AI chose ${ia_choice}, let's play !`);
+
+}
 
 shifumi();
